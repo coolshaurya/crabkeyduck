48c1edcc36bae919d3d6a223a8f95ff31cb0bcac
Shaurya Shubham <shauryashubham1234567890@gmail.com>
2021-01-21 11:53:27 +0000

    Fix compiler errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,29 @@
-#![derive(Debug, Clone)]
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
 struct ParsedDocument<'a> {
     title: Option<&'a str>,
     attributes: Attributes<'a>,
     elements: Vec<BlockElement<'a>>,
 }
 
-#![derive(Debug, Clone)]
+#[derive(Debug, Clone)]
 struct BlockElement<'a> {
     attributes: Attributes<'a>,
-    content: BlockContent,
+    content: BlockContent<'a>,
 }
 
-#![derive(Debug, Clone)]
-enum BlockContent {
-    Paragraph { text: Vec<InlineElement> },
+#[derive(Debug, Clone)]
+enum BlockContent<'a> {
+    Paragraph { text: Vec<InlineElement<'a>> },
 }
 
-#![derive(Debug, Clone)]
+#[derive(Debug, Clone)]
 enum InlineElement<'a> {
     Text(&'a str),
 }
 
-#![derive(Debug, Clone)]
+#[derive(Debug, Clone)]
 struct Attributes<'a> {
     attributes: HashMap<&'a str, &'a str>,
 }
