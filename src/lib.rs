306378c0eddbe81259c0f8892b903b42412e22a8
Shaurya Shubham <shauryashubham1234567890@gmail.com>
2021-01-21 10:15:28 +0000

    Draft out basic structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![derive(Debug, Clone)]
+struct ParsedDocument<'a> {
+    title: Option<&'a str>,
+    attributes: Attributes<'a>,
+    elements: Vec<BlockElement<'a>>,
+}
+
+#![derive(Debug, Clone)]
+struct BlockElement<'a> {
+    attributes: Attributes<'a>,
+    content: BlockContent,
+}
+
+#![derive(Debug, Clone)]
+enum BlockContent {
+    Paragraph { text: Vec<InlineElement> },
+}
+
+#![derive(Debug, Clone)]
+enum InlineElement<'a> {
+    Text(&'a str),
+}
+
+#![derive(Debug, Clone)]
+struct Attributes<'a> {
+    attributes: HashMap<&'a str, &'a str>,
 }
