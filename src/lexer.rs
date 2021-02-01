f009c9414b51a3d6737a7fcbf2d7b44754167b2e
Shaurya Shubham <shauryashubham1234567890@gmail.com>
2021-02-01 16:18:18 +0000

    Do basic lexing

--- src/lexer.rs
+++ src/lexer.rs
@@ -7,6 +7,33 @@ enum Token<'a> {
     HighlightDelimiter,
 }
 
-fn lex(text: &str) -> Vec<Token> {
-    todo!()
+fn lex(raw: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut anchor: usize = 0;
+    let iter = raw.char_indices().peekable();
+    for (i, ch) in iter {
+        match ch {
+            delim @ '*' | '_' | '`' | '#' => {
+                if anchor != i {
+                    tokens.push(Token::Text(raw[anchor..i]));
+                }
+                tokens.push(match delim {
+                    '*' => Token::BoldDelimiter,
+                    '_' => Token::ItalicsDelimiter,
+                    '`' => Token::MonospaceDelimiter,
+                    '#' => Token::HighlightDelimiter,
+                    _ => unreachable!(),
+                });
+                if let Some(&x) = iter.peek() {
+                    anchor = x.0;
+                }
+            }
+            _other => {
+                if iter.peek().is_none() {
+                    tokens.push(Token::Text(raw[anchor..]));
+                }
+            }
+        }
+    }
+    tokens
 }
